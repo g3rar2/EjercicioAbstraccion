d7aabcd85173d6f50d5e1ec81c68f83a935561ed
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-01 16:22:30 -0600

    adding menu

--- EjercicioAbstraccion/Program.cs
+++ EjercicioAbstraccion/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int opcion;
+
+Console.WriteLine("******************************");
+Console.WriteLine("             MENU             ");
+Console.WriteLine("* 1. Convertir Distancia     *");
+Console.WriteLine("* 2. Convertir Moneda        *");
+Console.WriteLine("* 3. Convertir Temperatura   *");
+Console.WriteLine("******************************");
+
+Console.WriteLine("Seleccione la opcion:");
+opcion = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+switch (opcion)
+{
+
+
+
+
+	default:
+		break;
+}
